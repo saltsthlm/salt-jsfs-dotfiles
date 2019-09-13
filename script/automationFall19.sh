9a2f75c086daa752cb37919a5952916ad934382d
Marcus Hammarberg <marcusoft.net@gmail.com>
2019-09-13 16:09:40 +0200

    Removed the UI

--- script/automationFall19.sh
+++ script/automationFall19.sh
@@ -3,27 +3,6 @@
 echo ">>>>Updating OSX stuff"
 sudo softwareupdate -i -a
 
-echo ">>>>Updating xcode without UI"
-
-# See http://apple.stackexchange.com/questions/107307/how-can-i-install-the-command-line-tools-completely-from-the-command-line
-
-echo "Checking Xcode CLI tools"
-# Only run if the tools are not installed yet
-# To check that try to print the SDK path
-xcode-select -p &> /dev/null
-if [ $? -ne 0 ]; then
-  echo "Xcode CLI tools not found. Installing them..."
-  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-  PROD=$(softwareupdate -l |
-    grep "\*.*Command Line" |
-    head -n 1 | awk -F"*" '{print $2}' |
-    sed -e 's/^ *//' |
-    tr -d '\n')
-  softwareupdate -i "$PROD" -v;
-else
-  echo "Xcode CLI tools OK"
-fi
-
 echo ">>>>>Installing homebrew"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
